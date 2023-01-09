480cab37f432fb055eeb2cfa2d352e7d418d7dcc
mn256od <martin.novysedlak.2@student.tuke.sk>
2023-01-09 20:30:04 +0100

    refactor and sending heartbeat works

--- node.c
+++ node.c
@@ -1,175 +1,25 @@
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <pthread.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/select.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include "node.h"
 
-#define MAXLINE 1024
-#define MAXNODES 6
-#define MSG_TYPE_HEARTBEAT 0
-#define MSG_TYPE_ORDINARY 1
-
-struct message
-{
-    uint8_t type;  // Message type (heartbeat or ordinary)
-    uint16_t data; // Data field (for ordinary messages)
-};
-
-struct parameters
-{
-    int socketfd_param;
-    uint16_t *known_ports_param;
-    size_t *known_ports_count_param;
-};
-
-void print_known_nodes(uint16_t *known_ports, size_t known_ports_count)
-{
-    for (size_t i = 0; i < known_ports_count; i++)
-    {
-        printf("\tknown node: %d\n", known_ports[i]);
-    }
-}
-
-int create_socket_and_bind(int port)
-{
-    // todo: sockopt
-    int socketfd = socket(AF_INET, SOCK_DGRAM, 0);
-
-    if (socketfd < 0)
-    {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    struct sockaddr_in servaddr;
-
-    memset(&servaddr, 0, sizeof(servaddr));
-
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = INADDR_ANY;
-    servaddr.sin_port = htons(port);
-
-    if (bind(socketfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    return socketfd;
-}
-
-void *listen_messages(void *arg)
-{
-    struct parameters *params = (struct parameters *)arg;
-    int socketfd = params->socketfd_param;
-    uint16_t *known_ports = params->known_ports_param;
-    size_t *known_ports_count = params->known_ports_count_param;
-
-    while (1)
-    {
-        char message[MAXLINE];
-
-        struct sockaddr_in cliaddr;
-        int len = sizeof(cliaddr);
-
-        int n = recvfrom(socketfd, (char *)message, MAXLINE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
-        message[n] = '\0';
-
-        char ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(cliaddr.sin_addr), ip, INET_ADDRSTRLEN);
-
-        uint16_t sender_port = ntohs(cliaddr.sin_port);
-
-        printf("sender address: %s:%d and he send this: %s\n", ip, sender_port, message);
-        // todo: distinguish heartbeat from message
-        // TODO: save port as known node (clear array and fill them with ports from heartbeat)
-    }
-    return NULL;
-}
-
-void send_message(int socketfd, char *message, uint16_t *known_ports, size_t known_ports_count)
-{
-    for (size_t i = 0; i < known_ports_count; i++)
-    {
-        struct sockaddr_in servaddr;
-
-        servaddr.sin_family = AF_INET;
-        servaddr.sin_addr.s_addr = INADDR_ANY;
-        servaddr.sin_port = htons(known_ports[i]);
-
-        sendto(socketfd, message, strlen(message), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-    }
-}
-
-void *send_heartbeat(void *arg)
-{
-    struct parameters *params = (struct parameters *)arg;
-    int socketfd = params->socketfd_param;
-    uint16_t *known_ports = params->known_ports_param;
-    size_t *known_ports_count = params->known_ports_count_param;
-
-    char *message = "heartbeat"; // todo: send known hosts
-
-    while (1)
-    {
-        sleep(10);
-
-        for (size_t i = 0; i < *known_ports_count; i++)
-        {
-            struct sockaddr_in servaddr;
-
-            servaddr.sin_family = AF_INET;
-            servaddr.sin_addr.s_addr = INADDR_ANY;
-            servaddr.sin_port = htons(known_ports[i]);
-
-            int n, len;
-
-            sendto(socketfd, message, strlen(message), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-
-            printf("heartbeat sent to %d\n", known_ports[i]);
-
-            print_known_nodes(known_ports, *known_ports_count);
-        }
-    }
-
-    return NULL;
-}
-
-// todo: remove this
-void *listen_heartbeat(void *arg)
-{
-    struct parameters *params = (struct parameters *)arg;
-    int socketfd = params->socketfd_param;
-    uint16_t *known_ports = params->known_ports_param;
-    size_t *known_ports_count = params->known_ports_count_param;
-
-    return NULL;
-}
+static int port;
 
 int main(const int argc, const char *argv[]) // todo: known hosts should not be allocated, nodes needs to send it todo: create struct of message, that will determine if thats heartbeat or message
 {
-    int port = atoi(argv[1]);
+    port = atoi(argv[1]);
     int port_to_connect = atoi(argv[2]);
 
     int socketfd;
-    struct parameters params;
+    Parameters params;
 
-    uint16_t *known_ports = (uint16_t *)calloc(MAXNODES, sizeof(uint16_t));
+    uint16_t *known_ports = (uint16_t *)calloc(MAXNODES + 1, sizeof(uint16_t)); // ensure that node has room for its own port to send over heartbeat
     size_t *known_ports_count = (size_t *)calloc(1, sizeof(size_t));
 
     if (port_to_connect != 0)
     {
-        known_ports[0] = 8081;
-        known_ports[1] = 8080;
-        *known_ports_count = 2;
-        // known_ports[*known_ports_count] = port_to_connect;
-        // *known_ports_count += 1;
+        // known_ports[0] = 8080;
+        // known_ports[1] = 8081;
+        // *known_ports_count = 2;
+        known_ports[*known_ports_count] = port_to_connect;
+        *known_ports_count += 1;
         // print_known_nodes(known_ports, *known_ports_count);
     }
 
@@ -182,7 +32,6 @@ int main(const int argc, const char *argv[]) // todo: known hosts should not be 
     pthread_t listen_thread_id, listen_heartbeat_thread_id, send_heartbeat_thread_id;
 
     pthread_create(&listen_thread_id, NULL, listen_messages, &params);
-    pthread_create(&listen_heartbeat_thread_id, NULL, listen_heartbeat, &params);
     pthread_create(&send_heartbeat_thread_id, NULL, send_heartbeat, &params);
 
     printf("Type help to get some help\n");
@@ -214,7 +63,6 @@ int main(const int argc, const char *argv[]) // todo: known hosts should not be 
             printf("\texiting...\n");
             free(known_ports);
             pthread_cancel(send_heartbeat_thread_id);
-            pthread_cancel(listen_heartbeat_thread_id);
             pthread_cancel(listen_thread_id);
             break;
         }
@@ -226,7 +74,196 @@ int main(const int argc, const char *argv[]) // todo: known hosts should not be 
 
     pthread_join(send_heartbeat_thread_id, NULL);
     pthread_join(listen_thread_id, NULL);
-    pthread_join(listen_heartbeat_thread_id, NULL);
 
     return 0;
+}
+
+void *listen_messages(void *arg)
+{
+    Parameters *params = (Parameters *)arg;
+    int socketfd = params->socketfd_param;
+    uint16_t *known_ports = params->known_ports_param;
+    size_t *known_ports_count = params->known_ports_count_param;
+
+    while (1)
+    {
+        Packet packet;
+
+        struct sockaddr_in cliaddr;
+        int len = sizeof(cliaddr);
+
+        int n = recvfrom(socketfd, &packet, sizeof(Packet), MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
+
+        char ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(cliaddr.sin_addr), ip, INET_ADDRSTRLEN);
+
+        uint16_t sender_port = ntohs(cliaddr.sin_port);
+
+        if (packet.type == MSG_TYPE_ORDINARY)
+        {
+            printf("sender address: %s:%d and he send this: %s\n", ip, sender_port, packet.data.message);
+        }
+        else if (packet.type == MSG_TYPE_HEARTBEAT)
+        {
+            printf("got heartbeat\n");
+            if (*known_ports_count == MAXNODES)
+            {
+                printf("this node has maximum count of known nodes!\n");
+                continue;
+            }
+
+            uint16_t *received_ports = calloc(MAXNODES, sizeof(uint16_t));
+
+            memcpy(received_ports, packet.data.ports, sizeof(packet.data.ports));
+
+            merge_known_ports(known_ports, *known_ports_count, received_ports, get_length(received_ports));
+            *known_ports_count = get_length(known_ports);
+
+            free(received_ports);
+        }
+        else
+        {
+            printf("cannot distinguish message type");
+        }
+    }
+    return NULL;
+}
+
+void send_message(int socketfd, char *message, uint16_t *known_ports, size_t known_ports_count)
+{
+    for (size_t i = 0; i < known_ports_count; i++)
+    {
+        struct sockaddr_in servaddr;
+
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_addr.s_addr = INADDR_ANY;
+        servaddr.sin_port = htons(known_ports[i]);
+
+        Packet packet;
+
+        packet.type = MSG_TYPE_ORDINARY;
+        memcpy(packet.data.message, message, strlen(message) + 1); // ensures terminator
+
+        sendto(socketfd, &packet, sizeof(packet), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    }
+}
+
+void *send_heartbeat(void *arg)
+{
+    Parameters *params = (Parameters *)arg;
+    int socketfd = params->socketfd_param;
+    uint16_t *known_ports = params->known_ports_param;
+    size_t *known_ports_count = params->known_ports_count_param;
+
+    while (1)
+    {
+        sleep(10);
+
+        for (size_t i = 0; i < *known_ports_count; i++)
+        {
+            struct sockaddr_in servaddr;
+
+            servaddr.sin_family = AF_INET;
+            servaddr.sin_addr.s_addr = INADDR_ANY;
+            servaddr.sin_port = htons(known_ports[i]);
+
+            Packet packet;
+
+            packet.type = MSG_TYPE_HEARTBEAT;
+            memcpy(packet.data.ports, known_ports, sizeof(known_ports));
+            append(packet.data.ports, port);
+
+            print_known_nodes(known_ports, get_length(known_ports));
+
+            sendto(socketfd, &packet, sizeof(packet), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+
+            printf("heartbeat sent to %d\n", known_ports[i]);
+
+            // print_known_nodes(known_ports, *known_ports_count);
+        }
+    }
+
+    return NULL;
+}
+
+int create_socket_and_bind(int port)
+{
+    // todo: sockopt
+    int socketfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (socketfd < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in servaddr;
+
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(port);
+
+    if (bind(socketfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return socketfd;
+}
+
+void print_known_nodes(uint16_t *known_ports, size_t known_ports_count)
+{
+    for (size_t i = 0; i < known_ports_count; i++)
+    {
+        printf("\tknown node: %d\n", known_ports[i]);
+    }
+}
+
+void merge_known_ports(uint16_t *known_ports, size_t known_ports_count, uint16_t *received_ports, size_t received_ports_count)
+{
+    for (size_t i = 0; i < received_ports_count; i++)
+    {
+        if (received_ports[i] == port)
+            continue;
+        bool is_present = false;
+        for (size_t j = 0; j < known_ports_count; j++)
+        {
+
+            if (known_ports[j] == received_ports[i])
+            {
+                is_present = true;
+                break;
+            }
+        }
+        if (!is_present)
+        {
+            append(known_ports, received_ports[i]);
+        }
+    }
+}
+
+void append(uint16_t *array, uint16_t port)
+{
+    size_t size = get_length(array);
+    if (size < MAXNODES)
+    {
+        array[size] = port;
+    }
+    else
+    {
+        printf("cannot append to array with maximum length");
+    }
+}
+
+size_t get_length(uint16_t *array)
+{
+    size_t size = 0;
+    while (array[size] != 0 && size < MAXNODES)
+    {
+        size++;
+    }
+    return size;
 }
---
+++ node.h
@@ -0,0 +1,52 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#define MAXLINE 1024
+#define MAXNODES 6
+
+#define MSG_TYPE_HEARTBEAT 0
+#define MSG_TYPE_ORDINARY 1
+
+typedef union Data
+{
+    char message[1024];
+    uint16_t ports[MAXNODES];
+} Data;
+
+typedef struct Packet
+{
+    uint8_t type;
+    Data data;
+} Packet;
+
+typedef struct Parameters
+{
+    int socketfd_param;
+    uint16_t *known_ports_param;
+    size_t *known_ports_count_param;
+} Parameters;
+
+
+int create_socket_and_bind(int port);
+size_t get_length(uint16_t *array);
+void append(uint16_t *array, uint16_t port);
+void *listen_messages(void *arg);
+void merge_known_ports(uint16_t *known_ports, size_t known_ports_count, uint16_t *received_ports, size_t received_ports_count);
+void print_known_nodes(uint16_t *known_ports, size_t known_ports_count);
+void *send_heartbeat(void *arg);
+void send_message(int socketfd, char *message, uint16_t *known_ports, size_t known_ports_count);
+
+#endif
