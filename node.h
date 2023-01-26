a37778ddf07804046dbd9f5ed7c284adbaea7f45
mn256od <martin.novysedlak.2@student.tuke.sk>
2023-01-26 15:57:39 +0100

    refactor

--- node.h
+++ node.h
@@ -43,15 +43,14 @@ typedef struct Parameters
     size_t *known_nodes_count_param;
 } Parameters;
 
-int create_socket_and_bind(int port);
-size_t get_count(uint16_t *array);
 size_t append(uint16_t *array, size_t *length, uint16_t port);
+int create_socket_and_bind(int port);
 void *listen_messages(void *arg);
 void merge_known_ports(uint16_t *known_nodes, size_t *known_nodes_count, uint16_t *received_ports);
 void print_known_nodes(uint16_t *known_nodes);
+void reduce_dead_nodes(uint16_t *known_nodes, size_t *known_nodes_count, struct timeval *known_nodes_alive_time);
 void *send_heartbeat(void *arg);
 void send_message(int socketfd, char *message, uint16_t *known_nodes);
-void reduce_dead_nodes(uint16_t *known_nodes, size_t *known_nodes_count, struct timeval *known_nodes_alive_time);
 void update_response_time(uint16_t *known_nodes, uint16_t sender_port, struct timeval *known_nodes_alive_time);
 
 #endif
